d3f90c6f69d9e5c1feba9983bcb1098ed5fc096f
HeylinPinedaSiu <heylinsiu06@gmail.com>
2024-05-05 14:07:34 -0600

    primer cambio

---
+++ GuiaClases_y_objetos/Empleado.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuiaClases_y_objetos
+{
+    internal class Empleado
+    {
+
+        //Variable miembro de la clase Empleado
+        private String nombre_Empleado;
+        private int numero_Empleado;
+        private decimal salario_Empleado;
+        private bool activoEmpleado; //Estado del empleado(activo o inactivo)
+
+
+        //creamos el constructor con los parametros
+        public Empleado(string nombre, int numero, decimal salario, bool activo )
+        {
+            this.nombre_Empleado = nombre;
+            this.numero_Empleado = numero;
+            this.salario_Empleado = salario;
+            this.activoEmpleado = activo;
+        }
+
+        //Metodo para mostrar los detalles del empleado
+        public void MostrarDetalles()
+        {
+            string estado = (activoEmpleado) ? "Activo" : "Inactivo";
+            Console.WriteLine($"Nombre: {nombre_Empleado}, Número de Empleado: {numero_Empleado}, Salario: {salario_Empleado:C}, Estado: {estado}");
+        }
+
+        //metodo para obtener el numero de empleado
+        public int ObtenerNumeroEmpleado()
+        {
+            return this.numero_Empleado;
+        }
+
+        //metodo para cambiar el estado del empleado
+        public void CambiarEstado(int nuevoEstado)
+        {
+            if (nuevoEstado == 1)
+            {
+                activoEmpleado = true; //Activar empleado
+                Console.WriteLine("Empleado Activado");
+            }
+            else if (nuevoEstado == 0)
+            {
+                activoEmpleado = false;//Desactivsr Empleado
+                Console.WriteLine("Empleado Desactivado");
+
+            }
+            else
+            {
+                Console.WriteLine("Estado no valido");
+            }
+            
+        
+        }
+    }
+}
+
---
+++ GuiaClases_y_objetos/Program.cs
@@ -0,0 +1,53 @@
+﻿using GuiaClases_y_objetos;
+
+internal class Program
+{
+    static void Main(String[] args)
+        { 
+
+        Console.WriteLine("***** Aplicación de gestión de empleados ***** ");
+         
+        //crear una lista para almacenar empleados
+        List<Empleado> listaEmpleados = new List<Empleado>();
+
+        //Agregar empleados de ejemplo a la lista
+        listaEmpleados.Add(new Empleado("Juan", 101, 30000, true));
+        listaEmpleados.Add(new Empleado("María", 102, 35000, true));
+        listaEmpleados.Add(new Empleado("Pedro", 103, 32000, false)); //empleado inactivo
+
+        //Mostrar los detalles de cada empleado en la lista 
+        foreach (var empleado in listaEmpleados)
+        {
+            empleado.MostrarDetalles();
+        }
+        //
+        //cambiar el estado de un empleado por su numero de empleado
+        int numeroEmpleadoCambiarEstado = 103;//Número de empleado a cambiar estado.
+        Empleado empleadoCambiarEstado = BuscarEmpleado(listaEmpleados, numeroEmpleadoCambiarEstado);
+
+        if (empleadoCambiarEstado != null)
+        {
+            //cambiar el estado a actualizar (1) o inactivo (0)
+            empleadoCambiarEstado.CambiarEstado(1);//cambiar activo
+            empleadoCambiarEstado.MostrarDetalles();//mostrar detalles actualizados
+        }
+        else
+        {
+            Console.WriteLine($"Empleado con numero {numeroEmpleadoCambiarEstado} no encontrado.");
+        }
+
+        //metodo para buscar un empleado por su numero de empleado
+        static Empleado BuscarEmpleado(List<Empleado> empleados, int numeroEmpleado )
+        {
+            foreach (var empleado in empleados)
+            {
+                if (empleado.ObtenerNumeroEmpleado() == numeroEmpleado)
+                {
+                    return empleado;
+                }
+            }
+            return null; 
+        }
+    }
+       
+}
